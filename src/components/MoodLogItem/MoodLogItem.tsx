2f85cf089e77251cc8113d69ee8a480de81f14df
Zora Peteri <zorapeteri@gmail.com>
2021-05-22 13:12:54 +0200

    fix moodlogitem longpress behaviour

--- src/components/MoodLogItem/MoodLogItem.tsx
+++ src/components/MoodLogItem/MoodLogItem.tsx
@@ -4,28 +4,46 @@ import moodNames from '../../moodNames';
 import useLongPress from '../../hooks/useLongPress';
 
 import { IoClose, IoTrashOutline } from 'react-icons/io5';
+import { format } from 'date-fns';
 
 type MoodLogItemProps = {
+  id: string;
   mood: Mood;
-  time: string;
+  time: Date;
+  onDelete: (id: string) => void;
   width?: string;
 };
 
 const MoodLogItem: React.FunctionComponent<MoodLogItemProps> = (props: MoodLogItemProps) => {
-  const { mood, time, width } = props;
+  const { id, mood, time, onDelete, width } = props;
 
   const [isLongPressed, setLongPressed] = useState<boolean>(false);
 
-  const longPress = { ...useLongPress(() => setLongPressed(true)), onBlur: () => setLongPressed(false) };
+  const setupListener = () => {
+    const listener = (e: MouseEvent) => {
+      console.log(e.target);
+      if (!(e.target as HTMLElement).matches(`.${style.handheldDeleteOverlay} *`)) {
+        setLongPressed(false);
+        document.removeEventListener('click', listener);
+      }
+    }
+    document.addEventListener('click', listener);
+  }
+
+  const longPress = { ...useLongPress(() => { setLongPressed(true); setupListener(); }) };
 
   return (
-    <div className={`${style.moodLogItem} ${isLongPressed && style.longPressed}`} style={{ maxWidth: width }} tabIndex={0} {...longPress}>
+    <div className={`${style.moodLogItem} ${isLongPressed && style.longPressed}`} style={{ maxWidth: width }} {...longPress}>
       <img src={`${process.env.PUBLIC_URL}/assets/moods/${mood}.png`} alt={`${moodNames[mood]} mood emoji icon`} />
-      <span>{time}</span>
+      <span>{format(time, 'hh:mm a')}</span>
       {isLongPressed && (
         <div className={style.handheldDeleteOverlay}>
           <div className={style.handheldDeleteOverlayBlock}></div>
-          <button className={style.handheldDeleteButton} title="delete this mood log item">
+          <button
+            className={style.handheldDeleteButton}
+            title="delete this mood log item"
+            onClick={() => onDelete(id)}
+          >
             <IoClose />
           </button>
         </div>
--- src/components/MoodLogItem/_stories_/MoodLogItem.stories.tsx
+++ src/components/MoodLogItem/_stories_/MoodLogItem.stories.tsx
@@ -8,5 +8,5 @@ import MoodLogItem from '../MoodLogItem';
 const stories = storiesOf('MoodLogItem', module);
 
 stories.add('MoodLogItem', () => {
-  return <MoodLogItem mood={1} time="09:00 AM" width="200px" />;
+  return <MoodLogItem id={'1'} mood={1} time={new Date()} onDelete={() => {}} width="200px" />;
 });
