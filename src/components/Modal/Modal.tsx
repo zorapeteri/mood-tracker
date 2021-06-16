1b06dfe4f4d3c70f3cf0f4fa8a1c9d38d294af7e
Zora Peteri <zorapeteri@gmail.com>
2021-06-16 21:26:01 +0200

    modal improvements

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,25 +9,27 @@ type ModalProps = {
   hideCloseButton?: boolean;
   buttonText: string;
   onButtonClick: () => void;
+  onClose: () => void;
   className?: string;
 };
 
 const Modal: React.FunctionComponent<ModalProps> = (props: ModalProps) => {
-  const { children, title, hideCloseButton, buttonText, onButtonClick, className } = props;
+  const { children, title, hideCloseButton, buttonText, onButtonClick, onClose, className } = props;
 
   return (
-    <div className={`${style.modal} ${className}`}>
-      <h2>{title}</h2>
-      <div className={style.body}>{children}</div>
-
-      <Button className={style.button} color="primary" padding="10px 25px" fontSize="12px" onClick={() => onButtonClick()}>
-        {buttonText}
-      </Button>
-      {!hideCloseButton && (
-        <button className={style.closeButton} title="close dialog">
-          <IoClose />
-        </button>
-      )}
+    <div className={style.modalOverlay}>
+        <div className={`${style.modal} ${className}`}>
+          <h2>{title}</h2>
+          <div className={style.body}>{children}</div>
+          <Button className={style.button} color="primary" padding="10px 25px" fontSize="12px" onClick={() => onButtonClick()}>
+            {buttonText}
+          </Button>
+          {!hideCloseButton && (
+            <button className={style.closeButton} title="close dialog" onClick={() => onClose()}>
+              <IoClose />
+            </button>
+          )}
+        </div>
     </div>
   );
 };
--- src/components/Modal/_stories_/Modal.stories.tsx
+++ src/components/Modal/_stories_/Modal.stories.tsx
@@ -10,7 +10,12 @@ const stories = storiesOf('Modal', module);
 stories.add('Modal', () => {
   return (
     <div style={{ width: '90vw', height: '50vh', display: 'grid', placeItems: 'center' }}>
-      <Modal title="Modal" buttonText="Okay" onButtonClick={() => alert('button clicked')}>
+      <Modal
+        title="Modal"
+        buttonText="Okay"
+        onButtonClick={() => alert('button clicked')}
+        onClose={() => console.log('closed')}
+      >
         <p>This is the content of the modal</p>
       </Modal>
     </div>
