50a8a24738e6f7cbb3c3ccb4808a7bb316ef13d6
Zora Peteri <zorapeteri@gmail.com>
2021-06-27 07:15:55 +0200

    make viewtoggle story interactive

--- src/components/ViewToggle/_stories_/ViewToggle.stories.tsx
+++ src/components/ViewToggle/_stories_/ViewToggle.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import '../../../scss/index.scss';
@@ -8,9 +8,12 @@ import ViewToggle from '../ViewToggle';
 const stories = storiesOf('ViewToggle', module);
 
 stories.add('ViewToggle', () => {
+
+  const [activeView, setActiveView] = useState<'today' | 'calendar'>('today');
+
   return (
     <div style={{ width: '350px' }}>
-      <ViewToggle activeToggle="today" onChange={() => {}} />
+      <ViewToggle activeToggle={activeView} onChange={option => setActiveView(option)}/>
     </div>
   );
 });
