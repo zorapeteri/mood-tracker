b04709711ce328968f52aa1592c06a888b47beaf
Zora Peteri <zorapeteri@gmail.com>
2021-09-23 22:57:38 +0200

    add tall option to textfield

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,15 +6,16 @@ type TextFieldProps = {
   rows: number;
   defaultValue?: string;
   onChange?: (text: string) => boolean | string;
+  tall?: boolean;
 };
 
 const TextField: React.FunctionComponent<TextFieldProps> = (props: TextFieldProps) => {
-  const { defaultValue, placeholder, onChange, rows } = props;
+  const { defaultValue, placeholder, onChange, rows, tall } = props;
 
   const [validationError, setValidationError] = useState<boolean | string>(false);
 
   return (
-    <div className={style.textField}>
+    <div className={`${style.textField} ${tall ? style.tall : ''}`}>
       <textarea
         defaultValue={defaultValue}
         maxLength={10000}
