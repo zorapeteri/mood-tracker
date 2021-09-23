8504bcc3833856ea574a9a52deca5a0886784638
Zora Peteri <zorapeteri@gmail.com>
2021-09-23 23:30:22 +0200

    make editname responsive

--- src/components/EditName/EditName.tsx
+++ src/components/EditName/EditName.tsx
@@ -1,38 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../../context/Context';
-import useBreakpoint from 'use-breakpoint';
-import BREAKPOINTS from '../../breakpoints';
-import HandheldEditName from './HandheldEditName';
-import DesktopEditName from './DesktopEditName';
+import Modal from '../Modal';
+import TextField from '../TextField';
 
 type EditNameProps = {
   close: () => void;
 };
 
-const EditName: React.FunctionComponent<EditNameProps> = (props: EditNameProps) => {
-  const { breakpoint } = useBreakpoint(BREAKPOINTS);
-
+const EditName: React.FunctionComponent<EditNameProps> = (
+  props: EditNameProps
+) => {
   const { userPreferences, setUserPreferences } = useContext(Context);
+
+  const [name, setName] = useState<string>(
+    (userPreferences as UserPreferences).name || ''
+  );
+
+  const onChange = (text: string) => {
+    setName(text);
+    return text ? false : 'You have to enter your name';
+  };
 
   const save = (name: string) => {
     setUserPreferences({ ...(userPreferences as UserPreferences), name });
     props.close();
   };
 
-  if (breakpoint === 'large') {
-    return (
-      <DesktopEditName
-        name={(userPreferences as UserPreferences).name || ''}
-        save={text => save(text)}
-      />
-    );
-  }
-
   return (
-    <HandheldEditName
-      name={(userPreferences as UserPreferences).name || ''}
-      save={text => save(text)}
-    />
+    <Modal
+      title="What would you like to be called?"
+      buttonText="Save"
+      onButtonClick={() => save(name)}
+      disableButton={!name}
+      hideCloseButton={true}
+    >
+      <TextField
+        placeholder="Your name"
+        rows={1}
+        defaultValue={(userPreferences as UserPreferences).name || ''}
+        onChange={onChange}
+      />
+    </Modal>
   );
 };
 
